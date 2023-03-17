90774767a8904e230d43e83406a2968fba0eeff9
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-03-17 19:28:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,50 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int c = 0;
+  for (int i = 0; i < len; i++){
+    for (int j = i+1; j < len; j++){
+      if (arr[i] + arr[j] == value)
+        c+=1;
+    }
+  } 
+  return c;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int c = 0;
+  for (int i = 0; i < len; i++){
+    for (int j = len; i < j; j--){
+      if (arr[i] + arr[j] == value)
+        c+=1;
+    }
+  } 
+  return c;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int c = 0;
+  for (int i = 0; i < len; i++){
+    int left = 0, right = len;
+    while (1 < right - left) {
+      int m = (left + right) / 2;
+      if (arr[i] + arr[m] == value) {
+          c++;
+          int t = m + 1;
+        while (arr[i] + arr[t] == value && t < right) {
+          c++;
+          t++;
+        }
+        t = m - 1;
+        while (arr[i] + arr[t] == value && t > left) {
+          c++;
+          t--;
+        }
+        break;
+      }
+      if (arr[i] + arr[m] > value) {
+        right = m;
+      } else {
+        left = m;
+      }
+    }
+  }
+  return c;
 }
